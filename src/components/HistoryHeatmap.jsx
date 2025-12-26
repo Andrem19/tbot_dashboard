d8c70ad4f79929fe99aec7f9177d80cc9bded4d7
Andrem19 <7255591@gmail.com>
2025-12-26 15:15:34 +0000

    f

--- src/components/HistoryHeatmap.jsx
+++ src/components/HistoryHeatmap.jsx
@@ -237,7 +237,7 @@ export default function HistoryHeatmap({ history = [] }) {
                  <>
                    <span className="hm-day-label">{new Date(cell.dateStr).getDate()}</span>
                    <div className="hm-profit-val">
-                      {Number(cell.profit).toFixed(2)}
+                      {cell.profit > 0 ? '+' : ''}{Number(cell.profit).toFixed(2)}
                    </div>
                  </>
               )}
